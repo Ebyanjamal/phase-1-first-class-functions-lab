713527426e477eb90dfd44d71f00790a1b490ba6
Ebyan Jamal <Ebyanjamal6@gmail.com>
2023-01-02 08:59:55 -0700

    commit tree 9790235392afc767096f5d881eb861f93c07996a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+ return ( ['Antonia', 'Nuru', 'Amari', 'Mo']).slice(0,2);
+}
+
+const returnLastTwoDrivers= function(){
+ return (['Antonia', 'Nuru', 'Amari', 'Mo']).slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers]; 
+
+let num1 = 5
+let num2 = 25
+const createFareMultiplier= function(num){
+  return function(num2){ 
+    return num1 * num2; 
+  }
+}; 
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
